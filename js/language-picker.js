19f241ad578d7cc8ccc1f48e47c2662da3be146e
Flo Hauck <mail@flohauck.de>
2022-02-11 14:45:24 +0100

    Cursor Color + Scroll to Top Button

--- js/language-picker.js
+++ js/language-picker.js
@@ -1,4 +1,6 @@
-var language = window.navigator.userLanguage || window.navigator.language;
+/*var language = window.navigator.userLanguage || window.navigator.language;
+
+
 // Browser-Sprache auslesen
 if(language.indexOf('de') !== -1) {
 	testsprache = 'de';
@@ -8,7 +10,29 @@ if(language.indexOf('de') !== -1) {
 // Browser-Sprache an den Body anhängen
 $('body').attr( "id",'lang-'+testsprache);
 // Sprache wechseln (hier mit jquery auf bestimmte Links)
+
+*/
+
+// URL auslesen und die einzelnen URL Teile in Array aufsplitten
+var pathArray = window.location.pathname.split('/');
+var secondLevelLocation = pathArray[1];
+
+// Abfrage ob die zweite Stelle der URL einen Language Code besitzt
+var selectedLanguage = 'en';
+
+if(secondLevelLocation === 'de' || secondLevelLocation === 'it'){
+	selectedLanguage = secondLevelLocation;
+}
+
+// Language Switcher mit korrekter Sprache Klasse active zuordnen
+$('.lang-switch li a.'+selectedLanguage).addClass('active');
+
+
+/*
+$('.lang-switch li a[lang="'+testsprache+'"]').addClass('active');
+
 $(".lang-switch").on('click',function(e){
 	e.preventDefault();
-$("body").attr( "id",'lang-'+$(this).attr('lang'));
+	$("body").attr( "id",'lang-'+$(this).attr('lang'));
 });
+*/
--- js/scripts.js
+++ js/scripts.js
@@ -44,7 +44,22 @@ jQuery(document).ready(function($) {
 			$('#loadingScreen').remove();
 		}
 	});
+	
+	$('#btn-scroll-top').click(function () { // Klick auf den Button
+	   $('body,html').animate({
+		  scrollTop: 0
+	   }, 800);
+	   return false;
+	});
 
+});
+
+$(window).scroll(function () {
+   if ($(this).scrollTop() > 100) { // Wenn 100 Pixel gescrolled wurde
+	  $('#btn-scroll-top').fadeIn();
+   } else {
+	  $('#btn-scroll-top').fadeOut();
+   }
 });
 
 
